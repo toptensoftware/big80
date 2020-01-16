935ff19da1f38175793eb55f257798b51f4acf5c
Brad Robinson <contact@toptensoftware.com>
2020-01-16 23:32:08 +1100

    Reflector basics working

---
+++ tools/reflector-client/reflector.js
@@ -0,0 +1,99 @@
+const ansiEscapes = require('ansi-escapes');
+const readline = require('readline');
+const SerialPort = require('serialport');
+
+// Clear screen and make room for status line
+process.stdout.write(ansiEscapes.clearScreen);
+process.stdout.write("\n\n");
+
+// Open Serial port
+const port = new SerialPort('COM4', {
+    baudRate: 115200
+})
+
+// Show status message
+function showStatus(msg)
+{
+    process.stdout.write(ansiEscapes.cursorHide);
+    process.stdout.write(ansiEscapes.cursorSavePosition);
+    process.stdout.write(ansiEscapes.cursorTo(0,0));
+    process.stdout.write(msg);
+    process.stdout.write(ansiEscapes.cursorRestorePosition);
+    process.stdout.write(ansiEscapes.cursorShow);
+}
+
+// Serial port data
+let bits = "";
+port.on('data', function (data) {
+
+    //console.log(data);
+
+    // Process incoming data
+    for (let i=0; i<data.length; i++)
+    {
+        // High-bit set = start of bit packet
+        if (data[i] & 0x80)
+            bits = "";
+
+        // Combine bit packet back into a binary string
+        bits = (data[i] & 0x7f).toString(2).padStart(7, '0') + bits;
+    }
+
+    // Prettify and show
+    if (bits.length > 16)
+        showStatus(bits.replace(/./g, x => x =='1' ? "●" : "-").substr(-16));
+});
+  
+
+
+// Readline console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
+});
+process.stdout.write(ansiEscapes.cursorTo(0,6));
+rl.prompt();
+rl.on('line', (line) => {
+
+    // Quit?
+    if (line.trim() == "exit")
+    {
+        rl.close();
+        return;
+    }
+
+    // Display command result
+    process.stdout.write(ansiEscapes.cursorTo(0,2));
+    process.stdout.write(ansiEscapes.eraseDown);
+    process.stdout.write(`Entered: '${line.trim()}'`);
+
+    // Prompt again
+    process.stdout.write(ansiEscapes.cursorTo(0,6));
+    rl.prompt();
+});
+rl.on('close', () => {
+    port.close();
+    process.stdout.write("\n\n");
+});
+
+/*
+
+## Display string format ##
+
+[000{7..0}000] - binary 10010001
+[{15..8}:x] - hex 
+[{7..0}:c] - ascii - "A"
+[{7..0}:l] - led eg: #--#---#
+[{7..0}:m:init,idle,tx,rx] - enumerated state
+
+
+## Serial Protocol ##
+
+High bit set indicates start of packet
+High bit clear indicates continuation of packet
+
+Remaining bit = bits being transmitted
+
+*/
+
