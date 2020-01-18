f2f9cf4a353efcf9b1b725862bcce15dc29909bb
Brad Robinson <contact@toptensoftware.com>
2020-01-18 17:15:22 +1100

    Tweaks

--- tools/reflector-client/sandbox.js
+++ tools/reflector-client/sandbox.js
@@ -1,6 +1,7 @@
 let Reflector = require('./Reflector');
 let ReflectorUI = require('./ReflectorUI');
 
+// Reflector establishes a connection with the FPGA over serial port
 let reflector = new Reflector({
     receiveBitCount: 16,
     sendBitCount: 20,
@@ -8,18 +9,22 @@ let reflector = new Reflector({
     portOptions: { 
         baudRate: 115200 
     },
-    receiveAccessors: {
+    receiveAccessors: {                 // Declare bit fields from FPGA
         "i_buttons": [ 15, 12 ],
         "i_counter": [ 7, 0 ],
     },
-    sendAccessors: {
+    sendAccessors: {                    // Declare bit fields to FPGA
         "o_leds": [ 7, 0 ],
         "o_counter": [ 19, 8 ],
     }
 });
 
+// The accessors declared above are now available as properties
+// For sendAccessors, changes will be automatically sent to the FPGA
+// For receiveAccessors, they'll update when those signals in the FPGA change
 reflector.o_leds = 0x01;
 
+// Use a timer to change things.
 let timer = setInterval(function() {
 
     // Counter
@@ -30,10 +35,18 @@ let timer = setInterval(function() {
 
 }, 500);
 
+
+// This event notifies us that either something sent or received changed
+// Note the use of reflector.formatXxx() methods to get values and format for display
 reflector.on('change', function() {
-    ui.showStatus(`  to FPGA: counter: ${reflector.formatHex("o_counter")} leds: ${reflector.formatLeds("o_leds")}\nfrom FPGA: counter: ${reflector.formatHex("i_counter")} buttons: ${reflector.formatLeds("i_buttons")}`);
+    let msg =  `  to FPGA: counter: ${reflector.formatHex("o_counter")} leds: ${reflector.formatLeds("o_leds")}\n`;
+        msg += `from FPGA: counter: ${reflector.formatHex("i_counter")} buttons: ${reflector.formatLeds("i_buttons")}`;
+    ui.showStatus(msg);
 });
 
+
+// This is a simple console UI that displays a status string at
+// the top of the screen and provides a prompt where commands can be entered
 let ui = new ReflectorUI();
 ui.on('close', () => {
     clearInterval(timer);
